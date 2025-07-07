ea89c3bdf8596fc0843406cac3c69288d9ed26e6
Yasir Tapar <123985425+YT-Vettel@users.noreply.github.com>
2025-07-07 11:41:27 +0300

    Update ORDER_ANALYSIS_BY_CITY.sql

--- ORDER_ANALYSIS_BY_CITY.sql
+++ ORDER_ANALYSIS_BY_CITY.sql
@@ -1,31 +1,31 @@
 SELECT
--- O.ID AS SÝPARÝÞID,                     -- Her bir sipariþin benzersiz ID'si
--- O.DATE_ AS SÝPARÝÞTARÝHÝ,             -- Sipariþin verildiði tarih
--- I.DATE_ AS FATURATARÝHÝ,             -- Sipariþe ait faturanýn kesildiði tarih
+-- O.ID AS ORDERID,                        -- Unique ID for each order
+-- O.DATE_ AS ORDERDATE,                   -- Date when the order was placed
+-- I.DATE_ AS INVOICEDATE,                 -- Date when the invoice was issued
 
--- DATEPART(YEAR,O.DATE_) AS YIL,       -- Sipariþin yýlý
--- DATEPART(MONTH,O.DATE_) AS AY,       -- Sipariþin ayý
--- DATEPART(HOUR,O.DATE_) AS SAAT,      -- Sipariþin saati
+-- DATEPART(YEAR,O.DATE_) AS YEAR,         -- Year of the order
+-- DATEPART(MONTH,O.DATE_) AS MONTH,       -- Month of the order
+-- DATEPART(HOUR,O.DATE_) AS HOUR,         -- Hour of the order
 
-CT.CITY, -- Þehir ismi
-AVG(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS ORTALAMA_SIPARIS_SURESI, -- Ortalama sipariþ süresi (saat olarak)
-MIN(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS ENKISA_TESLIMATSURESÝ_SAAT, -- En kýsa teslimat süresi
-MAX(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS ENUZUN_TESLIMATSURESÝ_SAAT, -- En uzun teslimat süresi
-SUM(O.TOTALPRICE) AS TOPLAM_SIPARIS_TUTARI,                       -- Þehre ait toplam sipariþ tutarý
-COUNT(O.ID) AS SIPARISSAYISI                                      -- Þehirdeki toplam sipariþ sayýsý
+CT.CITY,                                  -- City name
+AVG(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS AVERAGE_ORDER_DURATION, -- Average order duration (in hours)
+MIN(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS SHORTEST_DELIVERY_TIME_HOURS, -- Shortest delivery time
+MAX(DATEDIFF(HOUR,O.DATE_,I.DATE_)) AS LONGEST_DELIVERY_TIME_HOURS,  -- Longest delivery time
+SUM(O.TOTALPRICE) AS TOTAL_ORDER_AMOUNT,                          -- Total order amount per city
+COUNT(O.ID) AS TOTAL_ORDERS                                       -- Total number of orders in the city
 
 FROM ORDERS O
-	INNER JOIN USERS U ON U.ID = O.USERID                  -- Sipariþi veren kullanýcý bilgisi
-	INNER JOIN ADDRESS A ON A.ID = O.ADDRESSID             -- Sipariþin gönderildiði adres
-	INNER JOIN CITIES CT ON CT.ID = A.CITYID               -- Adrese karþýlýk gelen þehir bilgisi
-	INNER JOIN TOWNS T ON T.ID = A.TOWNID                  -- Ýlçe bilgisi (kullanýlmýyor ama JOIN edilmiþ)
-	INNER JOIN DISTRICTS D ON D.ID = A.DISTRICTID          -- Mahalle bilgisi (kullanýlmýyor ama JOIN edilmiþ)
-	INNER JOIN PAYMENTS P ON P.ORDERID = O.ID              -- Sipariþin ödeme bilgisi
-	INNER JOIN INVOICES I ON I.ORDERID = O.ID              -- Sipariþe ait fatura bilgisi
+    INNER JOIN USERS U ON U.ID = O.USERID                   -- Information of the user who placed the order
+    INNER JOIN ADDRESS A ON A.ID = O.ADDRESSID              -- Address where the order is delivered
+    INNER JOIN CITIES CT ON CT.ID = A.CITYID                -- City information corresponding to the address
+    INNER JOIN TOWNS T ON T.ID = A.TOWNID                    -- District information (not used but joined)
+    INNER JOIN DISTRICTS D ON D.ID = A.DISTRICTID            -- Neighborhood information (not used but joined)
+    INNER JOIN PAYMENTS P ON P.ORDERID = O.ID                -- Payment information for the order
+    INNER JOIN INVOICES I ON I.ORDERID = O.ID                -- Invoice information related to the order
 
-GROUP BY CT.CITY                                           -- Veriler þehir bazýnda gruplanýyor
+GROUP BY CT.CITY                                              -- Grouping data by city
 
--- DATEPART(YEAR,O.DATE_) ,DATEPART(MONTH,O.DATE_),DATEPART(HOUR,O.DATE_) -- Zaman bazlý grup için kullanýlabilir (þu an yorum satýrýnda)
--- HAVING AVG(DATEDIFF(HOUR,O.DATE_,I.DATE_))>=34          -- Ortalama teslimat süresi 34 saatten büyük olanlar (þu an devre dýþý)
+-- DATEPART(YEAR,O.DATE_), DATEPART(MONTH,O.DATE_), DATEPART(HOUR,O.DATE_) -- Can be used for time-based grouping (currently commented out)
+-- HAVING AVG(DATEDIFF(HOUR,O.DATE_,I.DATE_)) >= 34           -- Filtering orders with average delivery time greater than 34 hours (currently disabled)
 
-ORDER BY 6 DESC                                             -- TOPLAM_SIPARIS_TUTARI sütununa göre azalan þekilde sýralanýr
+ORDER BY 6 DESC                                               -- Orders results descending by TOTAL_ORDER_AMOUNT column
